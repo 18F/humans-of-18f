149ace42508034d5a09f93f8c13a472ccf03d75a
Atul Varma <varmaa@gmail.com>
2016-05-09 10:39:37 -0400

    Retrieve and show team members.

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,15 +1,44 @@
 import React = require('react');
 import ReactDOM = require('react-dom');
 
+import * as Team from './team';
+
 interface Props {
 }
 
 interface State {
+  teamMembers: Team.TeamMember[]
 }
 
 class App extends React.Component<Props, State> {
+  state = {
+    teamMembers: []
+  };
+  componentDidMount() {
+    // TODO: Deal with case where component is unmounted before
+    // the promise is fulfilled.
+
+    // TODO: Handle errors.
+    Team.get().then((members) => {
+      this.setState({
+        teamMembers: members
+      });
+    });
+  }
   render() {
-    return <h1>Humans of 18F</h1>;
+    return (
+      <div>
+        <h1>Humans of 18F</h1>
+        {this.state.teamMembers.map((member, i) => {
+          return (
+            <p key={i}>
+              <img src={member.image}/>
+              {member.full_name}
+            </p>
+          );
+        })}
+      </div>
+    );
   }
 }
 
---
+++ lib/team.ts
@@ -0,0 +1,47 @@
+import {Promise} from 'es6-promise';
+
+const BASE_URL = 'https://18f.gsa.gov/hub/';
+const TEAM_URL = BASE_URL + 'api/team/';
+const NO_IMG = BASE_URL + 'assets/images/team/logo-18f.jpg';
+
+export interface TeamMember {
+  full_name: String,
+  image: String,
+  location?: String,
+  bio?: String,
+  working_groups?: String[],
+  projects?: String[],
+  skills?: String[],
+  interests?: String[]
+}
+
+export function get(): Promise<TeamMember[]> {
+  let req = new XMLHttpRequest();
+
+  return new Promise((resolve, reject) => {
+    req.open('GET', TEAM_URL);
+    req.onload = () => {
+      if (req.status !== 200) {
+        return reject(new Error("Got HTTP " + req.status));
+      }
+
+      let team = JSON.parse(req.responseText);
+
+      let members = Object.keys(team).map((name) => {
+        let member = team[name] as TeamMember;
+
+        member.image = BASE_URL + member.image;
+
+        return member;
+      }).filter((member) => {
+        return member.image !== NO_IMG;
+      });
+
+      resolve(members);
+    };
+    req.onerror = () => {
+      reject(new Error("An error occurred"));
+    };
+    req.send(null);
+  });
+}
